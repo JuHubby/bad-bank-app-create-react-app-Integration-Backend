aca0a0cd8a8fa5f2e3f3468863ae3c00e9ca9e07
Julieth Kruse <yuli2546@hotmail.com>
2024-06-27 17:36:28 -0500

    back to credential.cert

--- server/admin.js
+++ server/admin.js
@@ -7,12 +7,12 @@ const admin = require('firebase-admin');
 
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  // credential: admin.credential.cert({
-  //   "projectId": process.env.FIREBASE_PROJECT_ID,
-  //   "private_key": process.env.FIREBASE_PRIVATE_KEY,
-  //   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  // }),
+  // credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 
   databaseURL: "https://bank-app-68fd5-default-rtdb.firebaseio.com",
 });

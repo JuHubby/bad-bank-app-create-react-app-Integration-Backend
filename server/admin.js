c916e7b3d1669050c08d3dc9920ec2013dcd9edf
Julieth Kruse <yuli2546@hotmail.com>
2024-06-27 17:42:38 -0500

    no quotation marks for env variables

--- server/admin.js
+++ server/admin.js
@@ -9,9 +9,9 @@ const admin = require('firebase-admin');
 admin.initializeApp({
   // credential: admin.credential.applicationDefault(),
   credential: admin.credential.cert({
-    "projectId": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privatekey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientemail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 
   databaseURL: "https://bank-app-68fd5-default-rtdb.firebaseio.com",

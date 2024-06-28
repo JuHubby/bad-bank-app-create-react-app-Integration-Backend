0623828ebd299c90cf01e11e5bcfcf1f25a5f230
Julieth Kruse <yuli2546@hotmail.com>
2024-06-27 21:59:55 -0500

    back to google credentals

--- server/admin.js
+++ server/admin.js
@@ -8,12 +8,12 @@ const admin = require('firebase-admin');
 // const firebase_private_key = String(firebase_private_key_b64);
 
 admin.initializeApp({
-  // credential: admin.credential.applicationDefault(),
-  credential: admin.credential.cert({
-    project_id: String(process.env.FIREBASE_PROJECT_ID),
-    private_key: String(process.env.FIREBASE_PRIVATE_KEY),
-    client_email: String(process.env.FIREBASE_CLIENT_EMAIL),
-  }),
+  credential: admin.credential.applicationDefault(),
+  // credential: admin.credential.cert({
+  //   project_id: String(process.env.FIREBASE_PROJECT_ID),
+  //   private_key: String(process.env.FIREBASE_PRIVATE_KEY),
+  //   client_email: String(process.env.FIREBASE_CLIENT_EMAIL),
+  // }),
 
   databaseURL: "https://bank-app-68fd5-default-rtdb.firebaseio.com",
 });

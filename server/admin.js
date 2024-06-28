9b78293d08627d463f29cf9c570ba9fa76cf544d
Julieth Kruse <yuli2546@hotmail.com>
2024-06-27 20:49:29 -0500

    add string method to all env var

--- server/admin.js
+++ server/admin.js
@@ -4,15 +4,15 @@ const admin = require('firebase-admin');
 
 
 
-const firebase_private_key_b64 = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n');
-const firebase_private_key = String(firebase_private_key_b64);
+// const firebase_private_key_b64 = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n');
+// const firebase_private_key = String(firebase_private_key_b64);
 
 admin.initializeApp({
   // credential: admin.credential.applicationDefault(),
   credential: admin.credential.cert({
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: firebase_private_key,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    project_id: String(process.env.FIREBASE_PROJECT_ID),
+    private_key: String(process.env.FIREBASE_PRIVATE_KEY),
+    client_email: String(process.env.FIREBASE_CLIENT_EMAIL),
   }),
 
   databaseURL: "https://bank-app-68fd5-default-rtdb.firebaseio.com",

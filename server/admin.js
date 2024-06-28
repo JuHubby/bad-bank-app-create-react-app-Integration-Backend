ce743dc2494ee3f7d9d35674cc832694ec5d6326
Julieth Kruse <yuli2546@hotmail.com>
2024-06-27 21:05:02 -0500

    added solution for multilines env variable in private key

--- server/admin.js
+++ server/admin.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 
-
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
 
 // const firebase_private_key_b64 = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n');
 // const firebase_private_key = String(firebase_private_key_b64);
@@ -11,14 +11,14 @@ admin.initializeApp({
   // credential: admin.credential.applicationDefault(),
   credential: admin.credential.cert({
     project_id: String(process.env.FIREBASE_PROJECT_ID),
-    private_key: String(process.env.FIREBASE_PRIVATE_KEY),
+    // private_key: String(process.env.FIREBASE_PRIVATE_KEY),'
+    privateKey,
     client_email: String(process.env.FIREBASE_CLIENT_EMAIL),
   }),
 
   databaseURL: "https://bank-app-68fd5-default-rtdb.firebaseio.com",
 });
 
-console.log("env.private_key:", process.env.FIREBASE_PRIVATE_KEY);
 
 module.exports= admin; 
 
